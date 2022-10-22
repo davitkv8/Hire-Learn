8b9dd66b288700636da74a5ad748b4da0bbb6a54
Davit <davit.kv8@gmail.com>
2022-10-22 04:28:17 +0400

    adding autocomplete

--- users/static/users/hashTags.js
+++ users/static/users/hashTags.js
@@ -10,12 +10,14 @@ function countTags(){
     tagNumb.innerText = maxTags - tags.length;
 }
 function createTag(){
+    console.log("CALLED!")
     ul.querySelectorAll("li").forEach(li => li.remove());
     tags.slice().reverse().forEach(tag =>{
         let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
         ul.insertAdjacentHTML("afterbegin", liTag);
     });
     countTags();
+    document.getElementById('suggestions').innerHTML = "";
 }
 function remove(element, tag){
     let index  = tags.indexOf(tag);
@@ -25,10 +27,14 @@ function remove(element, tag){
 }
 function addTag(e){
     if(e.key == "Enter"){
+        console.log(e);
+        console.log(e.target)
+        console.log(e.target.value)
         let tag = e.target.value.replace(/\s+/g, ' ');
         if(tag.length > 1 && !tags.includes(tag)){
             if(tags.length < 10){
                 tag.split(',').forEach(tag => {
+                    console.log(tag)
                     tags.push(tag);
                     createTag();
                 });
@@ -44,3 +50,61 @@ removeBtn.addEventListener("click", () =>{
     ul.querySelectorAll("li").forEach(li => li.remove());
     countTags();
 });
+
+
+
+
+
+(function () {
+  "use strict";
+  let inputField = document.getElementById('userInputTags');
+  let ulField = document.getElementById('suggestions');
+
+  let newElement = document.createElement("p");
+
+  // Adding some styles here
+  newElement.style.fontSize = "20px";
+  newElement.style.paddingTop = "15px";
+  ulField.style.border = "none";
+
+
+
+  const question = document.createTextNode("Do You Mean ? ");
+  newElement.appendChild(question);
+
+  inputField.addEventListener('input', changeAutoComplete);
+  ulField.addEventListener('click', selectItem);
+
+  function changeAutoComplete({ target }) {
+    let data = target.value;
+    ulField.innerHTML = ``;
+    if (data.length) {
+      let autoCompleteValues = autoComplete(data);
+      autoCompleteValues.forEach(value => { addItem(value); });
+      ulField.insertBefore(newElement, ulField.firstChild);
+    }
+  }
+
+  function autoComplete(inputValue) {
+    let destination = ["Italy", "Spain", "Portugal", "Brazil"];
+    return destination.filter(
+      (value) => value.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
+
+  function addItem(value) {
+    ulField.innerHTML = ulField.innerHTML +`<li>${value}</li>`;
+  }
+
+  function selectItem({ target }) {
+    if (target.tagName === 'LI') {
+      // inputField.value = target.textContent;
+      ulField.innerHTML = ``;
+      tags.push(target.textContent);
+      createTag();
+      inputField.value = "";
+
+    }
+  }
+
+})();
