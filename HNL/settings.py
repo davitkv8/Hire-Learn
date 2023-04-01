d15ffe8a45460231f23b3a8dcbb8c855245c8e04
Davit <davit.kv8@gmail.com>
2023-04-02 03:59:47 +0400

    first demo of dockerized app postgre + django

--- HNL/settings.py
+++ HNL/settings.py
@@ -94,10 +94,10 @@ DATABASES = {
     # postgres
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "hirenlearn",
-        "USER": "postgres",
-        "PASSWORD": "datodato123",
-        "HOST": "localhost",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "postgres",
         "PORT": 5432,
     }
 }
