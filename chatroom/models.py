cf17e127c675a4e2cba8885c265bcd1bf9c3df3c
Davit <davit.kv8@gmail.com>
2023-04-01 22:53:11 +0400

    some updates

--- chatroom/models.py
+++ chatroom/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from classroom.models import Relationship
+from django.utils import timezone
+from cryptography.fernet import Fernet
+import base64
 
 
-class ChatRoom(models.Model):
-    chatroom = models.CharField(max_length=55, unique=True, null=True)
-    # members = models.OneToOneField(Relationship, on_delete=models.SET_NULL)
+class MessageRoom(models.Model):
+    message = models.CharField(max_length=255, null=False, blank=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_receiver')
+    is_seen = models.BooleanField(default=False)
+    create_datetime = models.DateTimeField(default=timezone.now)
+    seen_datetime = models.DateTimeField(null=True)
 
-    def get_members(self):
-        self.members.objects.filter()
+    def __str__(self):
+        return f'from {self.sender} to {self.receiver} on {self.create_datetime}'
