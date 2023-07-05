716e32cc79f0bc43ed1e3f05c63784e2c9b18f28
Davit <davit.kv8@gmail.com>
2023-07-06 03:52:16 +0400

    change reqs

--- our_public_packages/setup.py
+++ our_public_packages/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="string-data-manipulators",
+    name="string-data-algorithms",
     version="0.1.0",
     url="https://github.com/davitkv8/Hire-Learn",
     author="Davit Kvaratskhelia",
