b49a2ad84486214e20e58e0750b959a44b9bf42d
Davit <davit.kv8@gmail.com>
2023-07-07 05:40:56 +0400

    Bump version: 0.1.1 → 0.1.2

--- our_public_packages/setup.py
+++ our_public_packages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="string-data-algorithms",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/davitkv8/Hire-Learn",
     author="Davit Kvaratskhelia",
     author_email="davit.kv8@gmail.com",
