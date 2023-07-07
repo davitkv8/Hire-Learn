e392c50e263ea47c6c8106040fa6332cb43edcae
Davit <davit.kv8@gmail.com>
2023-07-07 04:02:04 +0400

    some changes

--- main.tf
+++ main.tf
@@ -168,7 +168,7 @@ resource "aws_instance" "prod" {
               sudo yum install docker -y
               sudo yum install docker -y
               sudo usermod -a -G docker ec2-user
-              sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               sudo chmod +x /usr/local/bin/docker-compose
               sudo yum install nginx
               sudo systemctl start nginx
